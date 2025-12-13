29853b64c35193c3e161ef885364db4d17dfd95c
spargonaut <1243607+spargonaut@users.noreply.github.com>
2025-12-13 09:18:34 -0800

    start the demos in full-screen

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,13 +30,18 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    launchOptions: {
+        args: ['--start-maximized'],
+    },
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+          viewport: null
+      },
     },
 
     // {
